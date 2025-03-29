f9ede7521848ba63fd34e80d7aa8201e7bbeb82f
tayebi1379 <a9119amin@gmail.com>
2025-03-29 09:41:08 +0330

    Update bot.py
    5432

--- bot.py
+++ bot.py
@@ -13,6 +13,8 @@ CHANNEL_ID = '@tehrankhabari_ir'
 PHOTO_SEPEHR_WIFE = 'https://cdn.rokna.net/thumbnail/wOmsWjeamneO/yYGYIWiRH1jE7SFsFf8OS8GtVdPr30fs0wJj5HjN1IuvcJmljcN6H8bAsgVZzpzYCc2Paf9tWNyagVuk0QlPbNxB-KuYdy9P6xL39i3G-Q82HeI91mK-78F62Z5KWk3gNl6RwvjtxurVX_hzZe6NzQ,,/%D8%B3%D9%BE%D9%87%D8%B1+%D8%AD%DB%8C%D8%AF%D8%B1%DB%8C.jpg'
 PHOTO_SASY_CENSORED = 'https://cdn.rokna.net/thumbnail/mHTJunUTOoEL/yYGYIWiRH1jE7SFsFf8OS8GtVdPr30fs0wJj5HjN1IuvcJmljcN6H8bAsgVZzpzYCc2Paf9tWNyagVuk0QlPbNxB-KuYdy9P6xL39i3G-Q82HeI91mK-78F62Z5KWk3gWiOBV6O9LT4lqjAfWapFmw,,/%D8%B3%D8%A7%D8%B3%DB%8C+%D9%85%D8%A7%D9%86%DA%A9%D9%86.jpg'
 PHOTO_RONALDO_WIFE = 'https://cdn.rokna.net/servev2/f6VBCVS65xWu/Db2f077dXpA,/%D8%B1%D9%88%D9%86%D8%A7%D9%84%D8%AF%D9%88+%D9%88+%D9%87%D9%85%D8%B3%D8%B1%D8%B4.jpg'
+PHOTO_ALIDAEI_WIFE = 'https://cdn.pishnahadevizheh.com/servev2/KGj3qrulKNsb/MnvWRFh5dGY,/%D8%B9%D9%84%DB%8C+%D8%AF%D8%A7%DB%8C%DB%8C.jpg'
+
 
 # تابع بررسی عضویت کاربر در کانال
 async def check_membership(context, user_id):
@@ -29,7 +31,8 @@ async def show_main_menu(update, context):
     keyboard = [
         ["دیدن عکس زن سپهر حیدری"],
         ["دیدن عکس سانسوری ساسی"],
-        ["دیدن عکس رونالدو و زنش"]
+        ["دیدن عکس رونالدو و زنش"],
+        ["دیدن عکس علی دایی و و زنش"]
     ]
     reply_markup = ReplyKeyboardMarkup(
         keyboard,
@@ -93,6 +96,11 @@ async def handle_message(update, context):
         delete_message = await context.bot.send_message(chat_id=user_id, text="این عکس پس از ۳۰ ثانیه حذف می‌شود")
         asyncio.create_task(delete_after_delay(context.bot, user_id, photo_message.message_id, delete_message.message_id))
         
+    elif message_text == "دیدن عکس علی دایی و زنش":
+        photo_message = await context.bot.send_photo(chat_id=user_id, photo=PHOTO_ALIDAEI_WIFE)
+        delete_message = await context.bot.send_message(chat_id=user_id, text="این عکس پس از ۳۰ ثانیه حذف می‌شود")
+        asyncio.create_task(delete_after_delay(context.bot, user_id, photo_message.message_id, delete_message.message_id))
+
     else:
         await update.message.reply_text("لطفاً یکی از گزینه‌های منو رو انتخاب کنید!")
 
